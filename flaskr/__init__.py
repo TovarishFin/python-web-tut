d23522f16d73d295dcd5d0b4b855cf63aab982b3
Cody Lamson <tovarishfin@gmail.com>
2018-04-28 17:38:38 +0200

    app logic done...

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -23,8 +23,8 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/')
-    def index():
+    @app.route('/hello')
+    def hello():
         return 'hello world'
 
     from . import db
@@ -32,5 +32,9 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_ur_rule('/', endpoint='index')
 
     return app
